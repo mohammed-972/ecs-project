b15a5ee2cd48f132851554a7893de83739a86880
mohammed-972 <ikram30002@gmail.com>
2026-09-11 16:26:19 +0100

    create alb to ecs sg

--- infra/security_groups.tf
+++ infra/security_groups.tf
@@ -22,3 +22,36 @@ resource "aws_vpc_security_group_ingress_rule" "alb_https" {
   ip_protocol = "tcp"
   to_port     = 443
 }
+
+resource "aws_vpc_security_group_egress_rule" "alb_to_ecs" {
+  security_group_id = aws_security_group.alb.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 3000
+  ip_protocol = "tcp"
+  to_port     = 3000
+}
+
+
+resource "aws_security_group" "ecs" {
+  name        = "ecs-sg"
+  description = "allow from alb sg"
+  vpc_id      = aws_vpc.main.id
+
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ecs_from_alb" {
+  security_group_id            = aws_security_group.ecs.id
+  referenced_security_group_id = aws_security_group.alb.id
+
+  from_port   = 3000
+  ip_protocol = "tcp"
+  to_port     = 3000
+}
+
+resource "aws_vpc_security_group_egress_rule" "ecs" {
+  security_group_id = aws_security_group.ecs.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "-1"
+}
