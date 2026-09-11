bd23a19756b6bf8390ef78c568a792822a5c81c9
mohammed-972 <ikram30002@gmail.com>
2026-09-11 17:21:26 +0100

    create listener

---
+++ infra/alb.tf
@@ -0,0 +1,35 @@
+resource "aws_lb" "alb" {
+  name               = "threat-composer-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+
+}
+
+
+
+resource "aws_lb_target_group" "tg" {
+  name        = "threat-composer-tg"
+  port        = 3000
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
+
+  health_check {
+    path     = "/"
+    protocol = "HTTP"
+  }
+}
+
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
