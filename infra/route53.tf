f15d0900aeeaa8a892c7b0a44ca892609be8e3ee
mohammed-972 <ikram30002@gmail.com>
2026-09-22 11:44:34 +0100

    create record and route traffic to alb dns

--- infra/route53.tf
+++ infra/route53.tf
@@ -3,3 +3,15 @@ data "aws_route53_zone" "zone" {
   private_zone = false
 }
 
+resource "aws_route53_record" "record" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "tm.mohammedislam.uk"
+  type    = "A"
+
+
+  alias {
+    name = aws_lb.alb.dns_name
+    zone_id = aws_lb.alb.zone_id
+    evaluate_target_health = false
+  }
+}
