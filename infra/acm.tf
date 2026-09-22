c72ce979c280f855ff936b8607bc2bc9c716f4c8
mohammed-972 <ikram30002@gmail.com>
2026-09-22 12:50:28 +0100

    add validation record

--- infra/acm.tf
+++ infra/acm.tf
@@ -4,5 +4,10 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn = aws_acm_certificate.cert.arn
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [ 
+    aws_route53_record.validate_cert.fqdn
+   ]
 }
+
+
