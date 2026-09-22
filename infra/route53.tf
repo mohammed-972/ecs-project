de4016e425fd0f9e235167a2ad2aaef1b3e9b30f
mohammed-972 <ikram30002@gmail.com>
2026-09-22 12:41:30 +0100

    add record to validate cert

--- infra/route53.tf
+++ infra/route53.tf
@@ -5,7 +5,7 @@ data "aws_route53_zone" "zone" {
 
 resource "aws_route53_record" "record" {
   zone_id = data.aws_route53_zone.zone.zone_id
-  name    = "tm.mohammedislam.uk"
+  name    = aws_acm_certificate.cert.domain_name
   type    = "A"
 
 
@@ -14,4 +14,19 @@ resource "aws_route53_record" "record" {
     zone_id                = aws_lb.alb.zone_id
     evaluate_target_health = false
   }
+
+
+}
+
+resource "aws_route53_record" "validate_cert" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = one(aws_acm_certificate.cert.domain_validation_options).resource_record_name
+  type    = one(aws_acm_certificate.cert.domain_validation_options).resource_record_type
+  records = [
+    one(aws_acm_certificate.cert.domain_validation_options).resource_record_value
+
+  ]
+
+  ttl = 300
+
 }
