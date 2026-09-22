1004178bdc3779400a5b29fd726934deac8cd2e4
mohammed-972 <ikram30002@gmail.com>
2026-09-22 11:33:22 +0100

    request acm certificate

---
+++ infra/acm.tf
@@ -0,0 +1,8 @@
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "tm.mohammedislam.uk"
+  validation_method = "DNS"
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+}
